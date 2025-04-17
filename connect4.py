52df2fe8060f03ee170403f3a8852fdcbd5b97c9
ssmit877 <ssmit877@students.kennesaw.edu>
2025-04-17 19:19:59 -0400

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -6,4 +6,7 @@ def initialize_board():
 def is_move_valid(board, column, player):
   return board[0][column] == 0
 
-
+def drop_piece(board, column, player):
+  for row in range(5, -1, -1):
+    if board[row][column] == 0:
+      board

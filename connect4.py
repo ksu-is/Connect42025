abfe93133569906b3a71a0a887794b0be30ef4a2
ssmit877 <ssmit877@students.kennesaw.edu>
2025-05-05 02:41:02 -0400

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -1,27 +1,26 @@
 import numpy as np
 
 def initialize_board():
-  return np.zeros((6, 7), dtype=int)
+    return np.zeros((6, 7), dtype=int)
 
-def is_move_valid(board, column, player):
-  return board[0][column] == 0
+def is_move_valid(board, column):
+    return board[0][column] == 0
 
 def drop_piece(board, column, player):
-  for row in range(5, -1, -1):
-    if board[row][column] == 0:
-      board [row][column] = player
-      return True
-  return False
+    for row in range(5, -1, -1):
+        if board[row][column] == 0:
+            board[row][column] = player
+            return True
+    return False
 
 def check_win(board, player):
- 
-  for row in range(6):
-      for column in range(4):
+
+    for row in range(6):
+        for column in range(4):
             if all(board[row][column:column+4] == player):
                 return True
 
-
-for row in range(3):
+    for row in range(3):
         for column in range(7):
             if all(board[row:row+4, column] == player):
                 return True

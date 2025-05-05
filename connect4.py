c2e474947c8e8cdb384fefa80f6a22b1cf58b691
ssmit877 <ssmit877@students.kennesaw.edu>
2025-05-05 02:03:30 -0400

    Update connect4.py

--- connect4.py
+++ connect4.py
@@ -19,3 +19,24 @@ def check_win(board, player):
       for column in range(4):
             if all(board[row][column:column+4] == player):
                 return True
+
+
+for row in range(3):
+        for column in range(7):
+            if all(board[row:row+4, column] == player):
+                return True
+
+    for row in range(3):
+        for column in range(4):
+            if all(np.diag(board[row:row+4, column:column+4]) == player):
+                return True
+
+    for row in range(3, 6):
+        for column in range(4):
+            if all(np.diag(np.fliplr(board[row-3:row+1, column:column+4])) == player):
+                return True
+    return False
+
+def print_board(board):
+    for row in board:
+        print("|" + "|".join(map(str, row)) + "|")
